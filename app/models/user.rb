33924f18ed82b679ee149243d2404610c83b7b0f
Tom Herrick <therrick@gmail.com>
2015-05-24 09:56:02 -0400

    linking to/from upmin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,18 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, if: :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    role.to_sym == :admin
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,15 @@ describe User do
   it '#email returns a string' do
     expect(@user.email).to match 'user@example.com'
   end
+
+  describe '#admin?' do
+    it 'returns false for non-admin' do
+      expect(@user.admin?).to eq false
+    end
+
+    it 'returns true for admin' do
+      user2 = create(:user, :admin, email: 'admin@example.com')
+      expect(user2.admin?).to eq true
+    end
+  end
 end
