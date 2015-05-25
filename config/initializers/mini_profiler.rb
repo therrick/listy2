d1bbc64fa4d1498b92c4b5ddbefe08952c716136
Tom Herrick <therrick@gmail.com>
2015-05-25 14:38:23 -0400

    only load mini-profiler in dev env

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,6 +1,8 @@
-require 'rack-mini-profiler'
+if Rails.env.development?
+  require 'rack-mini-profiler'
 
-Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfilerRails.initialize!(Rails.application)
 
-Rails.application.middleware.delete(Rack::MiniProfiler)
-Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+  Rails.application.middleware.delete(Rack::MiniProfiler)
+  Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+end
