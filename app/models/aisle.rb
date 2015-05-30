7aaebc21f6e22b285380d961f048c5ace00f6d40
Tom Herrick <therrick@gmail.com>
2015-05-30 14:07:38 -0400

    order aisles by position by default

--- app/models/aisle.rb
+++ app/models/aisle.rb
@@ -1,6 +1,7 @@
 class Aisle < ActiveRecord::Base
   belongs_to :store
   has_many :items
+  default_scope { order(:position) }
   validates :store, presence: true
   validates :name, presence: true, uniqueness: { scope: :store_id }
 
