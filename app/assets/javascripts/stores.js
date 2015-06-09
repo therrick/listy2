d2c54171c7041a6231c177bf96ce9d189580f943
Tom Herrick <therrick@gmail.com>
2015-06-08 21:48:55 -0400

    up/down item list nav via keyboard

--- app/assets/javascripts/stores.js
+++ app/assets/javascripts/stores.js
@@ -4,7 +4,8 @@ $(document).ready( function () {
 
   // do a search on each key-press
   // add item form doubles search form filtering the item list
-  $('input#item_name').keyup(function () {
+  $('input#item_name').keyup(function (e) {
+    if (e.keyCode == 38 || e.keyCode == 40) return; // handled by up/down nav
     delay(function () {
       if (isXS()) {
         $('input#item_name').goTo();
@@ -25,6 +26,65 @@ $(document).ready( function () {
       '</div>';
 
   $.fn.modalmanager.defaults.resize = true;
+
+  // up/down navigation of item list
+  $("body#stores-show").on("keydown", function(e){
+    var selectedIndex = $("tr.item_list.found.selected").index();
+    if (selectedIndex < 0) {
+      selectedIndex = 0;
+    }
+    var maxIndex = $("tr.item_list").size();
+    var newIndex = null;
+    if(e.keyCode === 13) {
+      $('form#new_item').submit();
+    }
+    else if(e.keyCode === 38) {
+      // up
+      console.log("up.  selectedIndex=" + selectedIndex)
+      if (selectedIndex <= 1) {
+        newIndex = maxIndex;
+      } else {
+        newIndex = selectedIndex - 1;
+      }
+      while (!$($("tr.item_list")[newIndex - 1]).hasClass("found")) {
+        if (newIndex == selectedIndex) {
+          break;
+        }
+        newIndex -= 1;
+        if (newIndex <= 1) {
+          newIndex = maxIndex;
+        }
+      }
+    }
+    else if(e.keyCode === 40) {
+        // down
+        console.log("down.  selectedIndex=" + selectedIndex)
+        if (selectedIndex >= maxIndex) {
+          newIndex = 1;
+        } else {
+          newIndex = selectedIndex + 1;
+        }
+        while (!$($("tr.item_list")[newIndex - 1]).hasClass("found")) {
+          if (newIndex == selectedIndex) {
+            break;
+          }
+          newIndex += 1;
+          if (newIndex > maxIndex) {
+            newIndex = 0;
+          }
+        }
+    }
+    if (newIndex !== null) {
+      console.log("newIndex=" + newIndex)
+      $("tr.item_list.selected").removeClass("selected")
+      $($("tr.item_list")[newIndex - 1]).addClass("selected")
+      selectedValue = $($("tr.item_list.selected")[0].children[1]).html().replace(/<div.*\/div>/,'').trim();
+      $('input#item_name')[0].value = selectedValue;
+      e.preventDefault();
+    }
+    // left = 37
+    // right = 39
+   });
 });
 
 function popup_item_menu(store_id, item_id) {
@@ -51,7 +111,11 @@ function filter_items(selector, query) {
   }
 
   $(selector).each(function() {
-    ($(this).find("td:nth-child(2)").text().search(new RegExp(query, "i")) < 0) ? $(this).css("display", "none") : $(this).css("display", "table-row");
+    if ($(this).find("td:nth-child(2)").text().search(new RegExp(query, "i")) < 0) {
+      $(this).removeClass("found")
+    } else {
+      $(this).addClass("found");
+    }
   });
 }
 
